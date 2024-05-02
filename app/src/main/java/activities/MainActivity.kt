688842867594c1461a5616c7b914f6c96c126bfc
Junoiri <martasadlowska77@gmail.com>
2024-05-03 01:17:37 +0200

    Added User Profile activity with base functionality

--- app/src/main/java/activities/MainActivity.kt
+++ app/src/main/java/activities/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.example.mspp_project.R
 
 class MainActivity : AppCompatActivity() {
+
+    //TODO: Implement custom snackbar that will appear when not all information is filled in the User Profile
+    // activity - check the data with database
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
