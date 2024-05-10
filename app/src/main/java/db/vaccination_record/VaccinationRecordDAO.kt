f31f914804d0e2908d13126783cb24b80a4a4265
Polax2 <276087@student.pwr.edu.pl>
2024-05-10 05:59:43 +0200

    Update record

--- app/src/main/java/db/vaccination_record/VaccinationRecordDAO.kt
+++ app/src/main/java/db/vaccination_record/VaccinationRecordDAO.kt
@@ -1,7 +1,8 @@
 package db.vaccination_record
 
 interface VaccinationRecordDAO {
-    fun getRecord(user_id: Int): Set<VaccinationRecord?>?
+    fun getRecord(record_id: Int): VaccinationRecord?
+    fun getAllRecords(user_id: Int): Set<VaccinationRecord?>?
     fun insertRecord(vaccinationRecord: VaccinationRecord): Boolean
     fun deleteRecord(record_id: Int): Boolean
     fun updateRecord(record_id: Int, vaccinationRecord: VaccinationRecord): Boolean
