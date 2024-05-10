5bf9a4c02b949e005455abe4405e0f4e5b37c337
Junoiri <martasadlowska77@gmail.com>
2024-05-10 12:25:09 +0200

    Updated KDoc's for CalendarActivity

--- app/src/main/java/activities/VaccinationCalendarActivity.kt
+++ app/src/main/java/activities/VaccinationCalendarActivity.kt
@@ -29,6 +29,15 @@ import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
+/**
+ * This activity is responsible for managing the vaccination calendar.
+ *
+ * @property userEmail The email of the current user.
+ * @property monthYearText The TextView that displays the current month and year.
+ * @property calendarRecyclerView The RecyclerView that displays the calendar.
+ * @property selectedDate The currently selected date.
+ * @property bottomSheetDialogFragment The BottomSheetDialogFragment that displays the vaccinations for the selected date.
+ */
 @RequiresApi(Build.VERSION_CODES.O)
 class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemListener {
     private var userEmail = Firebase.auth.currentUser?.email.toString()
@@ -38,6 +47,9 @@ class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemL
 
     private var bottomSheetDialogFragment: VaccinationBottomSheetDialogFragment? = null
 
+    /**
+     * Initializes the activity view, sets up the toolbar, initializes the widgets, and sets the month view.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +60,9 @@ class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemL
         setMonthView()
     }
 
+    /**
+     * Sets up the toolbar with a back button and title.
+     */
     private fun setupToolbar() {
         val toolbar = findViewById<LinearLayout>(R.id.toolbar)
 
@@ -63,11 +78,17 @@ class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemL
         titleTextView.text = "Calendar"
     }
 
+    /**
+     * Initializes the widgets used in this activity.
+     */
     private fun initWidgets() {
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView)
         monthYearText = findViewById(R.id.monthYearTV)
     }
 
+    /**
+     * Sets the month view in the calendar.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     private fun setMonthView() {
         monthYearText.text = monthYearFromDate(selectedDate)
@@ -78,6 +99,13 @@ class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemL
         calendarRecyclerView.adapter = calendarAdapter
     }
 
+
+    /**
+     * Returns an ArrayList of days in the month for the given date.
+     *
+     * @param date The date to get the days of the month for.
+     * @return An ArrayList of days in the month for the given date.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     private fun daysInMonthArray(date: LocalDate): ArrayList<String> {
         val daysInMonthArray = ArrayList<String>()
@@ -98,24 +126,42 @@ class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemL
         return daysInMonthArray
     }
 
+    /**
+     * Returns a formatted string of the month and year from the given date.
+     *
+     * @param date The date to get the month and year from.
+     * @return A formatted string of the month and year from the given date.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     private fun monthYearFromDate(date: LocalDate): String {
         val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
         return date.format(formatter)
     }
 
+    /**
+     * Changes the selected date to the previous month and updates the month view.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     fun previousMonthAction(view: View) {
         selectedDate = selectedDate.minusMonths(1)
         setMonthView()
     }
 
+    /**
+     * Changes the selected date to the next month and updates the month view.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     fun nextMonthAction(view: View) {
         selectedDate = selectedDate.plusMonths(1)
         setMonthView()
     }
 
+    /**
+     * Handles the item click event for a day in the calendar.
+     *
+     * @param position The position of the clicked item in the adapter.
+     * @param dayText The text of the clicked day.
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onItemClick(position: Int, dayText: String?) {
         if (!dayText.isNullOrEmpty()) {
@@ -141,12 +187,25 @@ class VaccinationCalendarActivity : AppCompatActivity(), CalendarAdapter.OnItemL
         }
     }
 
+    /**
+     * Returns the ID of the user with the given email.
+     *
+     * @param email The email of the user.
+     * @return The ID of the user.
+     */
     private suspend fun getId(email: String): Int? {
         return withContext(Dispatchers.IO) {
             UserSF.getId(email)
         }
     }
 
+    /**
+     * Handles the data for the selected date, including scheduled vaccinations and vaccination records.
+     *
+     * @param selectedDateText The text of the selected date.
+     * @param scheduledVaccinations The scheduled vaccinations for the selected date.
+     * @param vaccinationRecords The vaccination records for the selected date.
+     */
     private fun handleData(
         selectedDateText: String,
         scheduledVaccinations: Set<ScheduledVaccination?>?,
